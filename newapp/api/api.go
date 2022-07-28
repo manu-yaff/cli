e2f54f50495dfb008cc283ed74c80d494c34be6c
manu-yaff <manuelyg2097@gmail.com>
2022-07-27 20:58:54 -0500

    Refactor: add clients endpoint

---
+++ newapp/api/api.go
@@ -0,0 +1,38 @@
+package api
+
+import (
+	s "client-server/server"
+	"net/http"
+
+	"github.com/gin-gonic/gin"
+)
+
+type ClientsResponse struct {
+	Name     string
+	Ip       string
+	Date     string
+	Channels []string
+}
+
+// define endpoints routes for the http server
+func SetupRoutes(router *gin.Engine, server *s.Server) {
+	router.GET("/clients", func(ctx *gin.Context) {
+		GetClients(ctx, server)
+	})
+}
+
+// retrieves all the clients currently connected to the tcp server
+func GetClients(c *gin.Context, serverInstance *s.Server) {
+	clients := serverInstance.Clients
+	var serverResponseObject []ClientsResponse
+	for _, c := range clients {
+		item := &ClientsResponse{
+			Name:     c.Name,
+			Ip:       c.Conn.RemoteAddr().String(),
+			Date:     c.Date,
+			Channels: c.Channels,
+		}
+		serverResponseObject = append(serverResponseObject, *item)
+	}
+	c.JSON(http.StatusOK, serverResponseObject)
+}
--- newapp/server-cli/main.go
+++ newapp/server-cli/main.go
@@ -1,11 +1,16 @@
 package main
 
 import (
+	api "client-server/api"
 	ch "client-server/channel"
 	cl "client-server/client"
 	req "client-server/request"
 	s "client-server/server"
+	"fmt"
 	"net"
+
+	"github.com/gin-contrib/cors"
+	"github.com/gin-gonic/gin"
 )
 
 func main() {
@@ -15,6 +20,15 @@ func main() {
 		Channels:       make(map[string]*ch.Channel),
 		CurrentRequest: make(chan req.Request),
 	}
+
+	// create http server for browser client
+	gin.SetMode(gin.ReleaseMode)
+	router := gin.New()
+	router.Use(cors.Default())
+	api.SetupRoutes(router, server)
+
+	go router.Run("localhost:8888")
+	fmt.Println("Http server listening on port: 8888")
 
 	// listen for commands from the client
 	go server.ReadClientRequest()
